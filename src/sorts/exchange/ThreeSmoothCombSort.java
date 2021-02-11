fc18a4470b7f761b006df6c8b8b2314cbb379f19
mg-2018 <alsrl6710@gmail.com>
2021-02-11 20:03:50 +0900

    v210211
    - tweaked 3-smooth comb sort

--- src/sorts/exchange/ThreeSmoothCombSort.java
+++ src/sorts/exchange/ThreeSmoothCombSort.java
@@ -25,10 +25,10 @@ final public class ThreeSmoothCombSort extends Sort {
 	@Override
 	public void runSort(int[] array, int length, int base) {
 		int[] gap = {31104, 27648, 26244, 24576, 23328, 20736, 19683, 18432, 17496, 16384, 15552, 
-				     13824, 13122, 12288, 11664, 10368, 9216, 8748, 8192, 7776, 6912, 6561, 5832,
+				     13824, 13122, 12288, 11664, 10368, 9216, 8748, 8192, 7776, 6912, 6561, 6144, 5832,
 				     5184, 4608, 4374, 4096, 3888, 3456, 3072, 2916, 2592, 2304, 2187, 2048,
 				     1944, 1728, 1536, 1458, 1296, 1152, 1024, 972, 864, 768, 729, 648, 576,
-				     512, 486, 432, 384, 324, 288, 256, 243, 216, 192, 144, 128, 108, 96, 81,
+				     512, 486, 432, 384, 324, 288, 256, 243, 216, 192, 162, 144, 128, 108, 96, 81,
 				     72, 64, 54, 48, 36, 32, 27, 24, 18, 16, 12, 9, 8, 6, 4, 3, 2, 1, 0};
 		
 		int i = 0;
@@ -42,7 +42,11 @@ final public class ThreeSmoothCombSort extends Sort {
 			else {
 				for(int j=0; j+gap[i] < length; j++) {
 					if(Reads.compareIndices(array, j, j+gap[i], 0.25, true) > 0)
-						Writes.swap(array, j, j+gap[i], 0.25, true, false);
+					{
+						if(gap[i] == 1) Writes.swap(array, j, j+gap[i], 2, true, false);
+						
+						else Writes.swap(array, j, j+gap[i], 0.75, true, false);
+					}
 				}
 				
 				i++;
